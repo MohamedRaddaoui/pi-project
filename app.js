9a0f53e202dcecd1fe261f23f1361c983d301bd7
MohamedRaddaoui <mohamedraddaoui05@gmail.com>
2025-03-02 22:43:07 +0100

    Remove server instance

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var http = require("http");
-var port = process.env.PORT || 3000;
 
 var indexRouter = require("./src/routes/index");
 var usersRouter = require("./src/routes/users");
@@ -22,11 +19,5 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
-var server = http.createServer(app);
-
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
 
 module.exports = app;
