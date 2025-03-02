f9829d66caaf490e91367dc3cc29c08ecae418c7
MohamedRaddaoui <mohamedraddaoui05@gmail.com>
2025-03-03 00:56:54 +0100

    Updating Cypress Config

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000", // Your backend API URL
+    specPattern: "tests/e2e/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: false,
   },
 });
