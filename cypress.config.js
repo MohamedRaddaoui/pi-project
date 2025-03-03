898636ad54f0c9a30962bd9a3e891aba2e16a2d2
MohamedRaddaoui <mohamedraddaoui05@gmail.com>
2025-03-03 02:13:54 +0100

    Modifying Cypress screenshot feature

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000", // Your backend API URL
     specPattern: "tests/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    screenshotOnRunFailure: true,
     supportFile: false,
   },
 });
