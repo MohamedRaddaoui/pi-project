8aa7bf40705ce56de052cd6e30a55cffa80ccf1c
eyaBrahmi <beya89031@gmail.com>
2025-03-15 15:07:41 +0100

    updated molels/project.js

--- src/models/project.js
+++ src/models/project.js
@@ -1,17 +1,58 @@
-const mongo=require('mongoose')
-const Schema= mongo.Schema
+
+const mongoose=require("mongoose")
+
+const Schema= mongoose.Schema
 const Project=new Schema({
-    name:String,
-    description:String,
-    startDate:Date,
-    endDate:Date,
-    ownerID:String,
-    team:Number,
-    status:String,
-    category:String,
-    users:[],
-    tasks:[]
+    title:{
+        type:String,
+        minlength: 3,
+        trim: true,
+        required:true,  
+        },
+
+    description:{
+        type:String,
+        minlength: 200,
+        trim:true,
+        required:true,
+    },
+    startDate:{
+        type:Date,
+        required:true,
+        },
+    endDate:{
+        type:Date,
+        required:true,
+    },
+    ownerID:{
+        //type: mongoose.Schema.ObjectId,
+        //ref: User
+    },
+    team:{
+        type: Number,
+        default: 0,
+    },
+    status:{
+        type: String,
+        trim: true,
+        enum: ['Not Started','In Progress', 'Done', 'Canceled'],
+        default: 'Not Started',
+    },
+    category:{
+        type:String,
+        required: true
+    },
+    usersID:[{
+        type:Array,
+        user: mongoose.Schema.ObjectId, 
+        ref : "User", 
+    }],
+    tasksID:[{
+        type:Array,
+          task: mongoose.Schema.ObjectId,
+          ref : "Tasks",
+    }]
 
 
 })
-module.exports=mongo.model('project', Project)
+module.exports=mongoose.model('project', Project)
