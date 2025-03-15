f2ce436dc19f99e54d471d0ec9984e1d9e41c4d5
eyaBrahmi <beya89031@gmail.com>
2025-03-15 15:08:24 +0100

    updated routes

--- src/routes/project.js
+++ src/routes/project.js
@@ -1,14 +1,18 @@
 var express = require("express");
 var router = express.Router();
 var projectCtrl = require("../controllers/projectController");
+const {
+    validateProject,
+    validateObjectId,
+
+}= require("../middlewares/projectValidation")
 
 
-
-router.post("/AddProject",projectCtrl.addProject)  //Add new project
-router.get("/ProjectByID/:id",projectCtrl.getProjectByID) //Get project by ID
+router.post("/AddProject",validateProject,projectCtrl.createProject)  //Add new project
+router.get("/ProjectByID/:id",validateObjectId,projectCtrl.getProjectByID) //Get project by ID
 router.get("/ListProject",projectCtrl.getAllProject) // Show all Project
-router.put("/UpdateProject/:id", projectCtrl.updateProject)  //update Project
-router.delete("/DeleteProject/:id",projectCtrl.deleteProject)  //Delete Project
+router.put("/UpdateProject/:id",validateObjectId, projectCtrl.updateProject)  //update Project
+router.delete("/DeleteProject/:id",validateObjectId,projectCtrl.deleteProject)  //Delete Project
 
 
 module.exports = router;
