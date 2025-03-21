bcbca3dca91fbe5752788ce569353b19a87368fb
NidhalxMRR <nidhalgharbi@gmail.com>
2025-03-21 23:14:20 +0100

    Feat/crudforum (#9)
    * crud forum
    
    * crud forum
    
    * crud forum
    
    ---------
    
    Co-authored-by: xZeOs <76686232+MohamedRaddaoui@users.noreply.github.com>

--- src/routes/project.js
+++ src/routes/project.js
@@ -4,15 +4,15 @@ var projectCtrl = require("../controllers/projectController");
 const {
     validateProject,
     validateObjectId,
+}= require("../middlewares/projectValidation");
 
-}= require("../middlewares/projectValidation")
 
+router.post("/AddProject",validateProject,projectCtrl.createProject);  //Add new project
+router.get("/ProjectByID/:id",validateObjectId,projectCtrl.getProjectByID); //Get project by ID
+router.get("/ListProject",projectCtrl.getAllProject); // Show all Project
+router.put("/UpdateProject/:id",validateObjectId, projectCtrl.updateProject);  //update Project
+router.delete("/DeleteProject/:id",validateObjectId,projectCtrl.deleteProject);  //Delete Project
 
-router.post("/AddProject",validateProject,projectCtrl.createProject)  //Add new project
-router.get("/ProjectByID/:id",validateObjectId,projectCtrl.getProjectByID) //Get project by ID
-router.get("/ListProject",projectCtrl.getAllProject) // Show all Project
-router.put("/UpdateProject/:id",validateObjectId, projectCtrl.updateProject)  //update Project
-router.delete("/DeleteProject/:id",validateObjectId,projectCtrl.deleteProject)  //Delete Project
 
 
 module.exports = router;
