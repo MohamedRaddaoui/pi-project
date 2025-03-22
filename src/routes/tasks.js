78f94bc3eeb24929c0a2d8aa7077f19f2a4453d6
Rabaa <rabaakhlifi@gmail.com>
2025-03-22 15:11:17 +0100

    update taskController urls

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -4,18 +4,18 @@ const taskController = require("../controllers/taskController");
 const { validateTask, validateObjectId } = require("../middlewares/validation");
 
 // Create a task
-router.post("/", validateTask, taskController.createTask);
+router.post("/create", validateTask, taskController.createTask);
 
 // Get all tasks
-router.get("/", taskController.getAllTasks);
+router.get("/getAll/", taskController.getAllTasks);
 
 // Get a single task by ID
-router.get("/:id", validateObjectId, taskController.getTaskById);
+router.get("/getbyId/:id", validateObjectId, taskController.getTaskById);
 
 // Update a task
-router.put("/:id", validateObjectId, validateTask, taskController.updateTask);
+router.put("/update/:id", validateObjectId, validateTask, taskController.updateTask);
 
 // Delete a task
-router.delete("/:id", validateObjectId, taskController.deleteTask);
+router.delete("/delete/:id", validateObjectId, taskController.deleteTask);
 
 module.exports = router;
